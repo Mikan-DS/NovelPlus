a14887321cc86d40802dc495499332af7ff04e31
Mikan <72257910+Mikan-DS@users.noreply.github.com>
2024-09-16 17:45:50 +0300

    fix

--- NovelPlus/urls.py
+++ NovelPlus/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     path('users/', include('users.urls')),
     path('api/', include('common.urls')),
 
-    re_path(r'(?:store|vacancy)?(?:/[^/]+)?/?', include('frontend.urls')),
+    re_path(r'(?:store|vacancy|[a-z]+)?(?:/[^/]+)?/?', include('frontend.urls')),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
