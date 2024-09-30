7e0a1691a288929f045a2ede7e1059fd619f8e79
Mikan <72257910+Mikan-DS@users.noreply.github.com>
2024-09-30 18:10:08 +0300

    NP-9 Отображение пользователя

--- users/models.py
+++ users/models.py
@@ -1,3 +1,5 @@
+import typing
+
 from django.contrib.auth.models import AbstractUser, UserManager
 from django.db import models
 
@@ -7,8 +9,35 @@ class User(AbstractUser):
 
     avatar = models.ImageField(upload_to='avatars/', blank=True, null=True, verbose_name='Аватарка')
     vk_user_id = models.IntegerField(blank=True, null=True, unique=True, verbose_name="ID VK")
+    description = models.TextField(default="", verbose_name='Описание профиля')
 
     class Meta:
         db_table = 'auth_user'
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
+
+    @property
+    def as_user_info_dict(self) -> dict:
+        return {
+            'username': self.username,
+            'firstName': self.first_name,
+            'lastName': self.last_name,
+            'email': self.email,
+            'avatar': self.avatar.url if self.avatar else None,
+            'isAdmin': self.is_staff,
+            'isAuthenticated': True,
+            'id': self.id
+        }
+
+    def get_user_page_info_dict(self, current_user_id: typing.Union[int, None]) -> dict:
+        return {
+            'username': self.username,
+            'firstName': self.first_name,
+            'lastName': self.last_name,
+            'email': self.email,
+            'avatar': self.avatar.url if self.avatar else None,
+            'description': self.description,
+            'isOwner': self.id == current_user_id,
+            'dateJoined': self.date_joined.timestamp(),
+            'id': self.id
+        }
